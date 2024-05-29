e7da3bd6ce4c3add4f58589564fdc2a89717807b
emilyye26 <131821743+emilyye26@users.noreply.github.com>
2024-05-29 13:54:37 -0400

    created form modal to pop up after "add event" button is click  enter the commit message for your changes. Lines starting

--- src/components/home/calendar/AddEvent.tsx
+++ src/components/home/calendar/AddEvent.tsx
@@ -1,14 +1,23 @@
-import React from "react";
-import { Button } from "@patternfly/react-core";
+import React, { useState } from "react";
+import { Button, Modal } from "@patternfly/react-core";
 import { useNavigate } from "react-router-dom";
+import EventForm from "./AddEventForm";
 
 function ViewAllEvents() 
-{
-  const navigate = useNavigate();
-  //navigate to allPosts
-//   const goToAnnouncements = () => {
-//     navigate("/all-announcements", {});
-//   };
+{  
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleButtonClick = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleSubmit = () => {
+        setIsModalOpen(false);
+    };
 
   return (
     <div className="container" style={{
@@ -17,13 +26,19 @@ function ViewAllEvents()
         alignItems: "center",
         width: 100,
       }}>
-      <Button
-        className="px-3 py-2 mb-2 pinned pf-u-text-center"
-        variant="primary"
-        // onClick={() => goToAnnouncements()}
+      <Button className="px-3 py-2 mb-2 pinned pf-u-text-center" variant="primary"
+        onClick={() => handleButtonClick()}
       >
         Add Event!
       </Button>
+      <Modal 
+      title = "Add Event!" 
+      isOpen={isModalOpen} 
+      onClose={closeModal}> 
+
+    <EventForm onSubmit={handleSubmit}/>
+
+      </Modal>
     </div>
   );
 }
