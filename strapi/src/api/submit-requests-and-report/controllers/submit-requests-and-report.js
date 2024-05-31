b951927309fa9b802aa0669b246f871affc62563
Nathan-JzSu <114895085+Nathan-JzSu@users.noreply.github.com>
2024-05-30 20:05:01 -0400

    tried to fix strapi error

--- strapi/src/api/submit-requests-and-report/controllers/submit-requests-and-report.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * submit-requests-and-report controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::submit-requests-and-report.submit-requests-and-report');
