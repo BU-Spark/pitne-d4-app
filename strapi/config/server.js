03546e35d10399caf6b6236171356844995107c5
Nathan-JzSu <114895085+Nathan-JzSu@users.noreply.github.com>
2024-06-07 21:23:10 -0400

    change

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1334),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['Fd04LfTPaocFuckfgrFeT9X7xNf5fEW/UDzvx8VVQ1M=', '98Nj01kIYoICBxE6mzA+H1hoWwLLPpJIMbnJ++aWGTw=']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
