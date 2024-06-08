816534b672a1d48037fc95cc5e279d488b0cae73
Nathan-JzSu <114895085+Nathan-JzSu@users.noreply.github.com>
2024-06-08 14:10:25 -0400

    immigrate architecture from firebase to strapi

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', ['Fd04LfTPaocFuckfgrFeT9X7xNf5fEW/UDzvx8VVQ1M=', '98Nj01kIYoICBxE6mzA+H1hoWwLLPpJIMbnJ++aWGTw=']),
+    keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
