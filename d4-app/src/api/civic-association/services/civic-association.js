41b767ab486582c110da21de87dd0ba98e97c73b
Rixhee <rishitbansals@gmail.com>
2024-06-10 11:32:43 -0400

    Tried to add strapi backend

---
+++ d4-app/src/api/civic-association/services/civic-association.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * civic-association service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::civic-association.civic-association');
