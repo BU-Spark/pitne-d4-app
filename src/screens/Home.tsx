cda1b622d457ac7a6e2587b5746f0988db312321
cs320p <ssomasundara@umass.edu>
2024-06-17 14:35:14 -0400

    Added address link to the footer

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -417,6 +417,7 @@ function Home() {
          <p>
             <a href="mailto:brian.worrell@boston.gov">Mail: brian.worrell@boston.gov</a>
             <a href="tel:+16176353131">Call: +1 617-635-3131</a>
+            <a href="https://www.google.com/maps/dir//5+Erie+St,+Dorchester,+MA+02121/@42.3266068,-71.1355474,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x89e37bc15204b3e5:0x4e18ab632ba37f9e!2m2!1d-71.0788007!2d42.303259?entry=ttu">District office: 5 Erie St, Dorchester, MA 02121</a>
           </p>
         </div>
       </div>
