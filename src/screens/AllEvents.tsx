aa266f96373c0670b0572098078e12730f2fc73b
Nathan-JzSu <114895085+Nathan-JzSu@users.noreply.github.com>
2024-06-18 10:49:32 -0400

    update calendar

--- src/screens/AllEvents.tsx
+++ src/screens/AllEvents.tsx
@@ -10,9 +10,7 @@ import Events from "../components/home/calendar/Calendar";
 import LogoBar from "../components/home/LogoBar";
 import MonthCalendar from "../components/home/calendar/MonthCalendar";
 import EventButton from "../components/home/calendar/AddEvent";
-import Announcement from "../components/home/announcements/Announcement";
-import ViewAllAnnouncements from "../components/home/announcements/ViewAllAnnouncements";
-import type { announData } from "./Home";
+import axios from "axios";
 // import { useIsManager } from "./Home";
 
 function AllEvents() {
@@ -22,27 +20,6 @@ function AllEvents() {
   const [calendarData, setCalendarData] = useState<calData[]>([]);
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
 
-  useEffect(() => {
-    const fetchCalendarData = async () => {
-      try {
-        const eventsCollection = collection(db, "events");
-        // Get all documents from the "events" collection
-        const eventsSnapshot = await getDocs(eventsCollection);
-        // Map through each document and get its data
-        const eventsList = eventsSnapshot.docs.map(doc => ({
-          ...doc.data()
-        })) as calData[];
-        //set the calendar data
-        console.log("events list");
-        if (auth.currentUser){console.log("User: " + auth.currentUser.email);}
-        else {console.log("User is null");}
-        setCalendarData(eventsList);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchCalendarData();
-  }, []);
 
   const handleDateChange = (date: Date) => {
     setSelectedDate(date);
@@ -51,6 +28,7 @@ function AllEvents() {
   // Filters events to match the user's selected date
   const filteredEvents = calendarData.filter(event => {
     const eventDate = new Date(event.attributes.date);
+    // console.log(` ${eventDate},  ${selectedDate},  ${event.attributes.date}`);
 
     return (
       // Returns an event if the date matches the selection
@@ -60,6 +38,36 @@ function AllEvents() {
     );
     
   });
+
+  const fetchEvents = async () => {
+    try {
+      const {
+        data: { data },
+      } = await axios.get("http://pitne-d4-app-strapi-production.up.railway.app/api/events?populate=*");
+      const fetchedEvents = data.map((item: any) => ({
+        id: item.id,
+        attributes: {
+          title: item.attributes.EventName,
+          body: item.attributes.Description, 
+          image: item.attributes.EventFlyer?.data && item.attributes.EventFlyer.data.length > 0
+          ? "http://pitne-d4-app-strapi-production.up.railway.app" + item.attributes.EventFlyer.data[0].attributes.url
+          : '',
+          date: item.attributes.EventDate,
+          location: item.attributes.Location,
+          time: item.attributes.Time,
+        }, 
+      }));
+      console.log(fetchedEvents);
+
+      setCalendarData(fetchedEvents);
+    } catch (error) {
+      console.error('Fetching events failed:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
 
   return (
     <div className="container">
@@ -72,12 +80,16 @@ function AllEvents() {
         <AngleLeftIcon size="md" onClick={() => navigate("/")} />
       </div>
       </div> */}
+
       <div className="top-heading">District 4 Events Calendar</div>
-      <MonthCalendar onDateChange={handleDateChange} />
+      <MonthCalendar onDateChange={handleDateChange} calendarData={calendarData}/>
+
       {/* <EventButton /> */}
-      {localStorage.getItem('isManager') === 'true' && <EventButton />} 
+      {/* {localStorage.getItem('isManager') === 'true' && <EventButton />} EST time zone */}
       <Events data={filteredEvents} />
 
+      <div className = "calendar-text">All Upcoming Events: </div>
+        <Events data={calendarData} />
     </div>
   );
 }
