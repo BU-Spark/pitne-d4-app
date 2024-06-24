868d33284d08e35f3d6bc06bda8e60574c39c0b6
Rixhee <rishitbansals@gmail.com>
2024-06-24 10:31:47 -0400

    Added strapi config

--- strapi/config/middlewares.js
+++ strapi/config/middlewares.js
@@ -2,7 +2,12 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000', 'https://pitne-d4-app-strapi-production.up.railway.app'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
---
+++ strapi/src/api/ne-issue/services/ne-issue.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ne-issue service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::ne-issue.ne-issue');
