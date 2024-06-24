7d08e8343297fe2f66830b72f4ae1fcd33ce5db4
cs320p <ssomasundara@umass.edu>
2024-06-24 14:54:12 -0400

    Revert "Merge branch 'test' into Sowrathi"
    This reverts commit 14ca2aade21bd57590967d2d9acf3154e81d3413, reversing
    changes made to 0f1e2325e5e42aa17efd55b3b86e102ed6937142.

--- strapi/src/api/development/services/development.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * development service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::development.development');
--- strapi/src/api/mailing-list/routes/mailing-list.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * mailing-list router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::mailing-list.mailing-list');
