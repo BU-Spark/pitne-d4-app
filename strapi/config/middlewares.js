6a8232ca04bce3f528af22ae5b17b82ebe71cddc
Rixhee <rishitbansals@gmail.com>
2024-06-24 11:46:33 -0400

    Update strapi config

--- strapi/config/middlewares.js
+++ strapi/config/middlewares.js
@@ -1,8 +1,15 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000'], // Replace with your frontend URL
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
