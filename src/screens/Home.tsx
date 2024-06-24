d827f2768bb7b4be7f4af7b23a65172257f5487e
cs320p <ssomasundara@umass.edu>
2024-06-24 09:33:50 -0400

    Update Home.tsx

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -467,12 +467,6 @@ function Home() {
                   Learn more
                 </button>
             </div>
-            <p className="councilor-description">
-              Councilor Brian Worrell has been dedicated to serving the community of District 4 for many years. His efforts focus on improving local infrastructure, increasing public safety, and ensuring that every voice in the district is heard and valued.
-            </p>
-            <button className="learn-more-button" onClick={() => window.location.href = '/client-info'}>
-              Learn more
-            </button>
           </div>
          </div>
 
