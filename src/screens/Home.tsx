de479026588dccd65a584092ffe68925cda3078a
Nathan-JzSu <114895085+Nathan-JzSu@users.noreply.github.com>
2024-06-24 20:54:38 -0400

    comment some unnecessary console log

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -91,16 +91,16 @@ function checkIfManager() {
 
         const querySnapshot = await getDocs(q);
         if (!querySnapshot.empty) {
-          console.log("User is logged in and is an admin.");
+          // console.log("User is logged in and is an admin.");
           localStorage.setItem('isManager', 'true');
           resolve(true);  // Resolve the promise with true
         } else {
-          console.log("User is logged in but is not an admin.");
+          // console.log("User is logged in but is not an admin.");
           localStorage.setItem('isManager', 'false');
           resolve(false); // Resolve the promise with false
         }
       } else {
-        console.log("No user is currently logged in.");
+        // console.log("No user is currently logged in.");
         localStorage.setItem('isManager', 'false');
         resolve(false); // Resolve the promise with false
       }
@@ -164,7 +164,7 @@ function Home() {
         });
 
         const data = await response.json();
-        console.log('Success:', data);
+        // console.log('Success:', data);
 
         if (response.status === 200 || response.status === 201) {
           setEmail(''); // Clear the input after successful subscription
@@ -233,11 +233,11 @@ function Home() {
           }));
           setPinned(transformedInterests);
         } else {
-          console.log("No such document!");
+          // console.log("No such document!");
         }
       })
       .catch((error) => {
-        console.log("Error getting document:", error);
+        // console.log("Error getting document:", error);
       });
   }, [db]);
 
@@ -246,7 +246,7 @@ function Home() {
       // Only fetch from Firestore if the local storage does not have the manager status
       if (localStorage.getItem('isManager') === null) {
         await checkIfManager();
-        console.log("called function check Manager status");
+        // console.log("called function check Manager status");
       }
     };
 
@@ -278,7 +278,7 @@ function Home() {
           ...doc.data()
         })) as calData[];
         //set the calendar data
-        console.log("events list");
+        // console.log("events list");
         setCalendarData(eventsList);
       } catch (error) {
         console.log(error);
@@ -297,7 +297,7 @@ function Home() {
           ...doc.data()
         })) as announData[];
         //set the calendar data
-        console.log("announ list");
+        // console.log("announ list");
         setAnnounData(announList);
       } catch (error) {
         console.log(error);
@@ -314,7 +314,7 @@ function Home() {
               setUpdateData(json.data);
             });
           } else {
-            console.log(`status code: ${res.status}`);
+            // console.log(`status code: ${res.status}`);
             setUpdateData([
               {
                 id: -1,
@@ -352,7 +352,7 @@ function Home() {
       const loadedDevelopments = snapshot.docs.map(doc => ({
         ...doc.data()
       })) as DevelopmentData[];
-      console.log(loadedDevelopments)
+      // console.log(loadedDevelopments);
       setDevelopmentData(loadedDevelopments);
     }
 
@@ -422,7 +422,7 @@ function Home() {
           time: item.attributes.Time,
         },
       }));
-      console.log(fetchedEvents);
+      // console.log(fetchedEvents);
       // console.log(image);
 
       setCalendarData(fetchedEvents);
