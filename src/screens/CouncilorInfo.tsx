372ad69b5697161ed1aec259c601c1cd3606771b
Nathan-JzSu <114895085+Nathan-JzSu@users.noreply.github.com>
2024-06-26 15:27:28 -0400

    Update CouncilorInfo.tsx

--- src/screens/CouncilorInfo.tsx
+++ src/screens/CouncilorInfo.tsx
@@ -2,22 +2,25 @@ import React, { useState, useEffect } from 'react';
 import NavBar from '../components/navbar/NavBar';
 import ClientImage from '../images/councilor_photo.png'; // Import the image
 import Footer from '../components/Footer'; // Import the Footer component
+import { set } from 'date-fns';
 
 const CouncilorInfo: React.FC = () => {
 
   const [councilorName, setCouncilorName] = useState('');
   const [councilorDescription, setCouncilorDescription] = useState('');
+  const [consilorImage, setConsilorImage] = useState('');
 
   useEffect(() => {
     const fetchCouncilorInfo = async () => {
       try {
-        const response = await fetch('https://pitne-d4-app-strapi-production.up.railway.app/api/councilor-infos', {
+        const response = await fetch('https://pitne-d4-app-strapi-production.up.railway.app/api/councilor-infos?populate=*', {
           method: 'GET',
         });
         if (response.ok) {
           const data = await response.json();
+          // console.log(`https://pitne-d4-app-strapi-production.up.railway.app`+ data.data[0].attributes.CouncilorImage.data.attributes.url);
           setCouncilorName(data.data[0].attributes.Name);
-          setCouncilorDescription(data.data[0].attributes.Info);
+          setConsilorImage(`https://pitne-d4-app-strapi-production.up.railway.app${data.data[0].attributes.CouncilorImage.data.attributes.url}`);
         }
       } catch (error) {
         console.error('Error fetching councilor info from Strapi:', error);
@@ -40,7 +43,7 @@ const CouncilorInfo: React.FC = () => {
           </div>
           <div className="top-heading" style={{ fontSize: '28px' }}>About the Councilor</div>
           <div className='p-4'>
-            <img src={ClientImage} alt="Councilor" style={{ maxWidth: '100%', height: 'auto' }} />
+            <img src={consilorImage} alt="Councilor" style={{ maxWidth: '100%', height: 'auto' }} />
           </div>
           <div className='left-align'> {/* Apply the left-align class here */}
             <h2><b>{councilorName}</b></h2> {/* Display the councilor's name */}
@@ -54,3 +57,7 @@ const CouncilorInfo: React.FC = () => {
 };
 
 export default CouncilorInfo;
+
+
+
+
